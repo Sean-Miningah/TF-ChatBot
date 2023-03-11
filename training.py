9f135a5eff3b9b3c5dff2d0da1a2e0182cfb72c8
Sean-Miningah <miningahsean@gmail.com>
2023-03-11 21:52:52 +0300

    Final Commit

--- training.py
+++ training.py
@@ -8,7 +8,7 @@ from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD 
+from tensorflow.keras.optimizers import legacy
 
 
 # nltk.download('punkt')
@@ -18,9 +18,16 @@ lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 
+# a list of tokenized words(patterns)
 words = []
+
+# A list of the tags
 classes = []
+
+# A list of sets containing a list of words and their tags/ classes
 documents = []
+
+# Letters to be ignored in when lemmatizing
 ignore_letters = ['?', '!', '.', ',']
 
 for intent in intents['intents']:
@@ -38,7 +45,7 @@ words = sorted(set(words))
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes,pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 
 training = []
@@ -47,6 +54,7 @@ output_empty = [0] * len(classes)
 for document in documents: 
     bag = []
     word_patterns = document[0]
+    #  lemmatization in linguistics is the process of grouping together the inflected forms of a word so they can be analysed as a single item
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words: 
         bag.append(1) if word in word_patterns else bag.append(0)
@@ -57,10 +65,25 @@ for document in documents:
 
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
 
-print(words)
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = legacy.SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
+# model.save('chatbot_model.model')
+model.save('chatbotmodel.h5', hist)
+
+print("Done")
